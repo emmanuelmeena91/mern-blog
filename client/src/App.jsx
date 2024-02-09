24380f0d1e6a8b8cb34502a2469e3bb312fb369c
emmanuelmeena91 <ep10293837465@gmail.com>
2024-02-09 19:51:26 +0300

    create header and style it use icon and flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,12 @@ import { SignUp } from './pages/SignUp'
 import { Dashboard } from './pages/Dashboard'
 import { Projects } from './pages/Projects'
 import { Login } from './pages/Login'
+import { Header } from './components/Header'
+
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />

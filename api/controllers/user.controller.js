5a1e5f5edd08d567fdb907e9fdf257b4f4ce61e2
emmanuelmeena91 <ep10293837465@gmail.com>
2024-02-14 17:18:07 +0300

    profile pg

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,6 +30,7 @@ export const updateUser = async (req, res, next) => {
         if(!req.body.username.match(/^[a-zA-Z0-9]+$/)){
             return next(errorHandler(400, 'Username can only contain letters and numbers '));
         }
+    }
         try {
             const updatedUser = await User.findByIdAndUpdate(
                 req.params.userId,
@@ -47,5 +48,5 @@ export const updateUser = async (req, res, next) => {
            next(error);
         }
         
-    }
-}
+    
+};

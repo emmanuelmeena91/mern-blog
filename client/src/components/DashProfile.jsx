48f1ce5b0201980e95c0ba6f29bac76654aea2c6
emmanuelmeena91 <ep10293837465@gmail.com>
2024-02-22 15:20:36 +0300

    create post page

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -20,9 +20,10 @@ import { updateStart,
        } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export const DashProfile = () => {
-    const {currentUser, error} = useSelector(state => state.user);
+    const {currentUser, error, loading } = useSelector(state => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -231,9 +232,26 @@ const handleSignout = async () =>{
         placeholder='password' 
         onChange={handleChange}
         />     
-      <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-        Update
+      <Button 
+      type='submit' 
+      gradientDuoTone='purpleToBlue'
+      outline 
+      disabled={loading || imageFileUploading}>
+       {loading ? 'Loading...' : 'Update'}
       </Button>
+      { currentUser.isAdmin && (
+        <Link to={'/create-post'}>
+        <Button
+        type='button'
+        gradientDuoTone='purpleToPink'
+        className='w-full
+        '>
+         Create a post 
+        </Button>
+        </Link>
+      
+      )
+      }
         </form>
         <div className='text-red-500 flex justify-between mt-5'>
           <span onClick={() =>setShowModal(true)} className='cursor-pointer'>Delete Your Account</span>
